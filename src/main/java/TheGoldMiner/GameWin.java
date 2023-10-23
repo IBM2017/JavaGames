6a6463c7a56f313c1c8a785dfd46929d5a26a4c4
Mr Liu <2389775135@qq.com>
2023-10-23 21:44:19 +0800

    创建Java游戏项目，并提交至GitHub

---
+++ src/main/java/TheGoldMiner/GameWin.java
@@ -0,0 +1,12 @@
+package TheGoldMiner;
+
+/**
+ * @Description: 游戏胜利界面
+ * @Title: GameWin
+ * @Package TheGoldMiner
+ * @Author: Mr Liu
+ * @Copyright 版权归**企业（或个人）所有
+ * @CreateTime: 2023/10/23 21:34
+ */
+public class GameWin {
+}
